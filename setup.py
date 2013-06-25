7733397471d5b08827c7a4cbcc4f4026c0d8689e
Tom Prince <tom.prince@ualberta.net>
2013-06-24 19:30:03 -0600

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='parsley-protocols',
+    version='0.0',
+    url='https://github.com/twisted/parsley-protocols',
+    description="Client for twisted's amp interface to trac",
+    license='MIT',
+    author='Shiyao Ma',
+    author_email='i@introo.me',
+    packages=['parseproto', 'parseproto.test'],
+    install_requires=[
+        'twisted >= 13.0.0',
+        'parsley >= 1.1',
+    ],
+    zip_safe=False,
+)
