296a449b1c442fa513484f8043b0a53990032d02
introom <i@introo.me>
2013-07-10 15:43:34 +0800

    update intnstring_receiver

--- parseproto/basic/protocol.py
+++ parseproto/basic/protocol.py
@@ -1,10 +1,13 @@
 
 
 import functools, os
+from struct import pack, calcsize
 
 # Twisted imports
 from twisted.internet import error
 from twisted.internet.protocol import Protocol
+# expedient import for _something
+from twisted.protocols.basic import _PauseableMixin, StringTooLongError
 
 # Parsley imports
 from ometa.protocol import ParserProtocol
@@ -109,3 +112,145 @@ class LineOnlyReceiver(Protocol):
     def connectionLost(self, reason):
         # a bit ugly
         self.parserProtocol.disconnecting = True
+
+
+
+class IntNStringReceiverBaseSender(object):
+    def __init__(self, transport):
+        self.transport = transport
+
+
+    def sendString(self, string, structFormat):
+        self.transport.write(pack(structFormat, len(string)) + string)
+
+
+
+class IntNStringReceiver(Protocol, _PauseableMixin):
+    MAX_LENGTH = 99999
+    _unprocessed = b''
+
+    # The to-be-deprecated recvd is eliminated here.
+
+    def updateReceiver(self, sender, parser):
+        self.sender = sender
+        self.parser = parser
+        return self
+
+    def connectionMade(self):
+        print("MMMMM AM I INVOKED.????????")
+        # avoid infinite recursion
+        if getattr(self, 'parserProtocol', None) is not None:
+            return
+
+        self.parserProtocol = ParserProtocol(
+            getGrammar(parseproto.basic, "intn_string_receiver"),
+            IntNStringReceiverBaseSender,
+            self.updateReceiver,
+            {}
+        )
+        # could be better for the next line
+        self.parserProtocol.transport = self.transport
+        self.parserProtocol.connectionMade()
+
+    def stringReceived(self, string):
+        """
+        Override this for notification when each complete string is received.
+
+        @param string: The complete string which was received with all
+            framing (length prefix, etc) removed.
+        @type string: C{bytes}
+        """
+        raise NotImplementedError
+
+
+    def lengthLimitExceeded(self, length):
+        """
+        Callback invoked when a length prefix greater than C{MAX_LENGTH} is
+        received.  The default implementation disconnects the transport.
+        Override this.
+
+        @param length: The length prefix which was received.
+        @type length: C{int}
+        """
+
+        self.transport.loseConnection()
+
+
+    def dataReceived(self, data):
+        """
+        Convert int prefixed strings into calls to stringReceived.
+        """
+        self._unprocessed += data
+        if self.paused:
+            return
+        # we should make ParserProtocol be able to pause when new data is added
+        self.parserProtocol.dataReceived(self._unprocessed)
+        self._unprocessed = b''
+
+
+    def sendString(self, string):
+        """
+        Send a prefixed string to the other end of the connection.
+
+        @param string: The string to send.  The necessary framing (length
+            prefix, etc) will be added.
+        @type string: C{bytes}
+        """
+        if len(string) >= 2 ** (8 * self.prefixLength):
+            raise StringTooLongError(
+                "Try to send %s bytes whereas maximum is %s" % (
+                len(string), 2 ** (8 * self.prefixLength)))
+        self.sender.sendString(string, self.structFormat)
+
+
+    def getStringLength(self, slen):
+        length = 0
+        for s in slen:
+            length = length << 8 | ord(s)
+        return length
+
+
+    def checkStringLength(self, length):
+        return length < self.MAX_LENGTH
+
+
+class Int32StringReceiver(IntNStringReceiver):
+    """
+    A receiver for int32-prefixed strings.
+
+    An int32 string is a string prefixed by 4 bytes, the 32-bit length of
+    the string encoded in network byte order.
+
+    This class publishes the same interface as NetstringReceiver.
+    """
+    structFormat = "!I"
+    prefixLength = calcsize(structFormat)
+
+
+
+class Int16StringReceiver(IntNStringReceiver):
+    """
+    A receiver for int16-prefixed strings.
+
+    An int16 string is a string prefixed by 2 bytes, the 16-bit length of
+    the string encoded in network byte order.
+
+    This class publishes the same interface as NetstringReceiver.
+    """
+    structFormat = "!H"
+    prefixLength = calcsize(structFormat)
+
+
+
+class Int8StringReceiver(IntNStringReceiver):
+    """
+    A receiver for int8-prefixed strings.
+
+    An int8 string is a string prefixed by 1 byte, the 8-bit length of
+    the string.
+
+    This class publishes the same interface as NetstringReceiver.
+    """
+    structFormat = "!B"
+    prefixLength = calcsize(structFormat)
+
--- parseproto/test/test_basic.py
+++ parseproto/test/test_basic.py
@@ -1,13 +1,17 @@
 
 from __future__ import division, absolute_import
+import struct
 
 from twisted.python.compat import iterbytes
 from twisted.trial import unittest
 from twisted.internet import error
 from twisted.test import proto_helpers
+from twisted.protocols.test.test_basic import LPTestCaseMixin
 
 
-from parseproto.basic.protocol import LineOnlyReceiver
+from parseproto.basic.protocol import LineOnlyReceiver, IntNStringReceiver
+from parseproto.basic.protocol import (
+    Int8StringReceiver, Int16StringReceiver, Int32StringReceiver)
 
 class LineOnlyTester(LineOnlyReceiver):
     """
@@ -69,3 +73,225 @@ class LineOnlyReceiverTestCase(unittest.SynchronousTestCase):
         """
         proto = LineOnlyReceiver()
         self.assertRaises(NotImplementedError, proto.lineReceived, 'foo')
+
+
+class TestMixin:
+
+    def connectionMade(self):
+        self.received = []
+
+
+    def stringReceived(self, s):
+        self.received.append(s)
+
+    MAX_LENGTH = 50
+    closed = 0
+
+
+    def connectionLost(self, reason):
+        self.closed = 1
+
+
+class IntNTestCaseMixin(LPTestCaseMixin):
+    """
+    TestCase mixin for int-prefixed protocols.
+    """
+
+    protocol = None
+    strings = None
+    illegalStrings = None
+    partialStrings = None
+
+    def test_receive(self):
+        """
+        Test receiving data find the same data send.
+        """
+        r = self.getProtocol()
+        for s in self.strings:
+            for c in iterbytes(struct.pack(r.structFormat,len(s)) + s):
+                r.dataReceived(c)
+        self.assertEqual(r.received, self.strings)
+
+
+    def test_partial(self):
+        """
+        Send partial data, nothing should be definitely received.
+        """
+        for s in self.partialStrings:
+            r = self.getProtocol()
+            for c in iterbytes(s):
+                r.dataReceived(c)
+            self.assertEqual(r.received, [])
+
+
+    def test_send(self):
+        """
+        Test sending data over protocol.
+        """
+        r = self.getProtocol()
+        r.sendString(b"b" * 16)
+        self.assertEqual(r.transport.value(),
+            struct.pack(r.structFormat, 16) + b"b" * 16)
+
+
+    def test_lengthLimitExceeded(self):
+        """
+        When a length prefix is received which is greater than the protocol's
+        C{MAX_LENGTH} attribute, the C{lengthLimitExceeded} method is called
+        with the received length prefix.
+        """
+        length = []
+        r = self.getProtocol()
+        r.lengthLimitExceeded = length.append
+        r.MAX_LENGTH = 10
+        r.dataReceived(struct.pack(r.structFormat, 11))
+        self.assertEqual(length, [11])
+
+
+    def test_longStringNotDelivered(self):
+        """
+        If a length prefix for a string longer than C{MAX_LENGTH} is delivered
+        to C{dataReceived} at the same time as the entire string, the string is
+        not passed to C{stringReceived}.
+        """
+        r = self.getProtocol()
+        r.MAX_LENGTH = 10
+        r.dataReceived(
+            struct.pack(r.structFormat, 11) + b'x' * 11)
+        self.assertEqual(r.received, [])
+
+
+    # won't throw here because the parser covers it.
+    def test_stringReceivedNotImplemented(self):
+        """
+        When L{IntNStringReceiver.stringReceived} is not overridden in a
+        subclass, calling it raises C{NotImplementedError}.
+        """
+
+        # proto = basic.IntNStringReceiver()
+        # self.assertRaises(NotImplementedError, proto.stringReceived, 'foo')
+
+
+class TestInt32(TestMixin, Int32StringReceiver):
+    """
+    A L{basic.Int32StringReceiver} storing received strings in an array.
+
+    @ivar received: array holding received strings.
+    """
+
+
+
+class Int32TestCase(unittest.SynchronousTestCase, IntNTestCaseMixin):
+    """
+    Test case for int32-prefixed protocol
+    """
+    protocol = TestInt32
+    strings = [b"a", b"b" * 16]
+    illegalStrings = [b"\x10\x00\x00\x00aaaaaa"]
+    partialStrings = [b"\x00\x00\x00", b"hello there", b""]
+
+    def test_data(self):
+        """
+        Test specific behavior of the 32-bits length.
+        """
+        r = self.getProtocol()
+        r.sendString(b"foo")
+        self.assertEqual(r.transport.value(), b"\x00\x00\x00\x03foo")
+        r.dataReceived(b"\x00\x00\x00\x04ubar")
+        self.assertEqual(r.received, [b"ubar"])
+
+
+
+class TestInt16(TestMixin, Int16StringReceiver):
+    """
+    A L{Int16StringReceiver} storing received strings in an array.
+
+    @ivar received: array holding received strings.
+    """
+
+
+
+class Int16TestCase(unittest.SynchronousTestCase, IntNTestCaseMixin):
+    """
+    Test case for int16-prefixed protocol
+    """
+    protocol = TestInt16
+    strings = [b"a", b"b" * 16]
+    illegalStrings = [b"\x10\x00aaaaaa"]
+    partialStrings = [b"\x00", b"hello there", b""]
+
+    def test_data(self):
+        """
+        Test specific behavior of the 16-bits length.
+        """
+        r = self.getProtocol()
+        r.sendString(b"foo")
+        self.assertEqual(r.transport.value(), b"\x00\x03foo")
+        r.dataReceived(b"\x00\x04ubar")
+        self.assertEqual(r.received, [b"ubar"])
+
+
+    def test_tooLongSend(self):
+        """
+        Send too much data: that should cause an error.
+        """
+        r = self.getProtocol()
+        tooSend = b"b" * (2**(r.prefixLength * 8) + 1)
+        self.assertRaises(AssertionError, r.sendString, tooSend)
+
+
+
+# is this test case really needed? new style should be default.
+class NewStyleTestInt16(TestInt16, object):
+    """
+    A new-style class version of TestInt16
+    """
+
+
+
+class NewStyleInt16TestCase(Int16TestCase):
+    """
+    This test case verifies that IntNStringReceiver still works when inherited
+    by a new-style class.
+    """
+    protocol = NewStyleTestInt16
+
+
+
+class TestInt8(TestMixin, Int8StringReceiver):
+    """
+    A L{Int8StringReceiver} storing received strings in an array.
+
+    @ivar received: array holding received strings.
+    """
+
+
+
+class Int8TestCase(unittest.SynchronousTestCase, IntNTestCaseMixin):
+    """
+    Test case for int8-prefixed protocol
+    """
+    protocol = TestInt8
+    strings = [b"a", b"b" * 16]
+    illegalStrings = [b"\x00\x00aaaaaa"]
+    partialStrings = [b"\x08", b"dzadz", b""]
+
+
+    def test_data(self):
+        """
+        Test specific behavior of the 8-bits length.
+        """
+        r = self.getProtocol()
+        r.sendString(b"foo")
+        self.assertEqual(r.transport.value(), b"\x03foo")
+        r.dataReceived(b"\x04ubar")
+        self.assertEqual(r.received, [b"ubar"])
+
+
+    def test_tooLongSend(self):
+        """
+        Send too much data: that should cause an error.
+        """
+        r = self.getProtocol()
+        tooSend = b"b" * (2**(r.prefixLength * 8) + 1)
+        self.assertRaises(AssertionError, r.sendString, tooSend)
