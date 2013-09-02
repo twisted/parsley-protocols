944d2c2b3e97c4f40312726620122c70881866c6
introom <i@introo.me>
2013-09-02 21:16:10 +0800

    add profile file

---
+++ parseproto/profile/profile.py
@@ -0,0 +1,16 @@
+from parseproto.test.test_basic import LineReceiver, proto_helpers
+
+def test_maximumLineLength():
+    """
+    C{LineReceiver} disconnects the transport if it receives a line longer
+    than its C{MAX_LENGTH}.
+    """
+    proto = LineReceiver()
+    transport = proto_helpers.StringTransport()
+    proto.makeConnection(transport)
+    proto.dataReceived(b'x' * (proto.MAX_LENGTH + 1) + b'\r\nr')
+
+
+
+if __name__ == '__main__':
+    test_maximumLineLength()
